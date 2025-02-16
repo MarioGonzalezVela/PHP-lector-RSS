73ec9494a3f54e63a869342d77f1fad82396d5fe
Mario <mario.gonzalez@live.u-tad.com>
2025-02-16 22:42:10 +0100

    trying

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,12 +1,23 @@
 <?php
 
 $Repit = false;
-$conn_string = "postgres://neondb_owner:npg_5hXTxH1EuvpK@ep-calm-wildflower-a2d4l9gj-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
+
+if (function_exists('pg_connect')) {
+    echo "La extensión pgsql está habilitada.";
+} else {
+    echo "La extensión pgsql NO está habilitada.";
+}
+// Cadena de conexión
+$conn_string = "postgres://neondb_owner:npg_i1FdrVEhtW6R@ep-restless-haze-a2wi8ueb-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
+
+// Conectar a PostgreSQL
 $link = pg_connect($conn_string);
 
-if (!$link) {
-    die('Error: ' . pg_last_error());
+if (! $link) {
+    die("Error en la conexión: " . pg_last_error());
 }
 
+// Configurar codificación de caracteres a UTF8
 pg_set_client_encoding($link, "UTF8");
-echo "Conexión exitosa";
+
+echo "Conexión a PostgreSQL exitosa.";
